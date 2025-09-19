75864b5defc0a26bc83aeb094295f360cf4b3473
Chingosoft <asmansoft@mail.ru>
2025-09-19 22:24:26 +0600

    Add files via upload

---
+++ js/sliders.js
@@ -0,0 +1,28 @@
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation:{
+        prevEl: ".prev",
+        nextEl: ".next"
+    },
+
+    loop: true
+});
+
+const heroSwipe = new Swiper(".hero-slider", {
+    // slidesPerView: 1,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+   autoplay: {
+    delay: 3000,
+},
+    effect: 'fade',
+    
+    loop: true
+});
